7214cd14beeecdcb9473b7542071416214e5cf3b
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2021-01-14 15:14:18 +0100

    Added ce30_recorder.cpp

---
+++ ce30_driver/custom.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <ce30_driver/ce30_driver.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <ce30_driver/custom.h>
+
+#include "../csc/csc_debug.h"
+#include "../csc/csc_argv.h"
+
+#define CE30_WIDTH 320
+#define CE30_HIEGHT 20
+#define CE30_XY_INDEX(x,y) ((x)*CE30_HIEGHT + (y))
+
+enum ce30_outformat
+{
+	CE30_OUTFORMAT_TEXT_XYZA,
+	CE30_OUTFORMAT_BINARY_XYZA
+};
+
+
+
+
+
+
+void ce30_scan_to_frame (ce30_driver::Scan scan, float frame[CE30_WIDTH*CE30_HIEGHT*4])
+{
+	for (int x = 0; x < CE30_WIDTH; ++x)
+	{
+		for (int y = 0; y < CE30_HIEGHT; ++y)
+		{
+			ce30_driver::Channel channel = scan.at(x, y);
+			frame[CE30_XY_INDEX(x,y) + 0] = channel.point().x;
+			frame[CE30_XY_INDEX(x,y) + 1] = channel.point().x;
+			frame[CE30_XY_INDEX(x,y) + 2] = channel.point().x;
+			frame[CE30_XY_INDEX(x,y) + 3] = channel.amplitude;
+		}
+	}
+}
+
+
--- recording/ce30_recorder.cpp
+++ recording/ce30_recorder.cpp
@@ -2,24 +2,30 @@
 #include <ce30_driver/ce30_driver.h>
 #include <unistd.h>
 #include <time.h>
+
+#include <ce30_driver/custom.h>
+
 #include "../csc/csc_debug.h"
 #include "../csc/csc_argv.h"
 
 using namespace std;
 using namespace ce30_driver;
 
-#define ARG_HELP 0x00000001
+#define ARG_HELP    UINT32_C(0x00000001)
+#define ARG_VERBOSE UINT32_C(0x00000002)
 
 int main (int argc, char const * argv[])
 {
 	char const * arg_filename = "ce30_pointcloud.out";
 	double arg_duration = 0;
-	int arg_flags = 0;
+	uint32_t arg_flags = 0;
+	FILE * file_out = NULL;
 	struct csc_argv_option option[] =
 	{
 	{'d', "duration", CSC_ARGV_TYPE_DOUBLE,    &arg_duration,   {.val_umax = 0}, "How long to record"},
 	{'f', "filename", CSC_ARGV_TYPE_STRING,    &arg_filename,   {.val_umax = 0}, "The filename"},
-	{'h', "help",     CSC_ARGV_TYPE_INT,       &arg_flags,      {.val_int = ARG_HELP}, "Show help"},
+	{'h', "help",     CSC_ARGV_TYPE_U32,       &arg_flags,      {.val_u32 = ARG_HELP}, "Show help"},
+	{'v', "verbose",  CSC_ARGV_TYPE_U32,       &arg_flags,      {.val_u32 = ARG_VERBOSE}, "Show verbose"},
 	{.type = CSC_ARGV_TYPE_END}};
 
 	csc_argv_parsev (option, argv+1);
@@ -30,6 +36,16 @@ int main (int argc, char const * argv[])
 		csc_argv_print_value (option);
 		return 0;
 	}
+
+
+	if (arg_filename)
+	{
+		printf ("Opening binary file %s to write LiDAR frames.\n", arg_filename);
+		file_out = fopen (arg_filename, "wb");
+	}
+
+	ASSERT (file_out);
+
 
 	UDPSocket socket;
 	if (!Connect(socket))
@@ -59,57 +75,68 @@ int main (int argc, char const * argv[])
 		return -1;
 	}
 
-	// Now it's ready to receive measurement data
-	Packet packet;
-	Scan scan;
-
-
 	//Timestamps to measure duration of recording
 	struct timespec ts0;
 	struct timespec ts1;
+	double d10;
+	double d21;
+	uint32_t counter = 0;
+	Packet packet;
+	Scan scan;
 
-	//ASSERT_NOTNULL (arg_filename);
-	//FILE * f = fopen (arg_filename, "r");
-	//ASSERT_NOTNULL (f);
-
+	if (arg_flags & ARG_VERBOSE)
+	{
+		printf ("%10s %10s %10s %10s %10s\n", "frame", "duration", "spf", "fps", "avg_fps");
+	}
 	clock_gettime (CLOCK_REALTIME, &ts0);
+	clock_gettime (CLOCK_REALTIME, &ts1);
 	while (true)
 	{
 		if (!GetPacket(packet, socket))
 		{
 			continue;
 		}
-		unique_ptr<ParsedPacket> parsed = packet.Parse();
-		if (parsed)
+		std::unique_ptr<ParsedPacket> parsed = packet.Parse();
+		if (parsed == NULL)
 		{
-			scan.AddColumnsFromPacket(*parsed);
-			if (!scan.Ready())
-			{
-				continue;
-			}
-			for (int x = 0; x < scan.Width(); ++x)
-			{
-				for (int y = 0; y < scan.Height(); ++y)
-				{
-					Channel channel = scan.at(x, y);
-					//printf ("%f %f %f %02x\n", channel.point().x, channel.point().y, channel.point().z, channel.amp_raw);
-					printf ("%+4.2f %+4.2f %+4.2f %+4.2f %02x\n", channel.point().x, channel.point().y, channel.point().z, channel.amplitude, channel.amp_raw);
-					//fwrite ();
-				}
-			}
-			scan.Reset();
+			continue;
+		}
+		scan.AddColumnsFromPacket(*parsed);
+		if (!scan.Ready())
+		{
+			continue;
+		}
+
+		//fprintf (f, "%+Ef %+Ef %+Ef %+Ef %02x\n", channel.point().x, channel.point().y, channel.point().z, channel.amplitude, channel.amp_raw);
+
+		float frame[CE30_WIDTH*CE30_HIEGHT*4] = {0};
+		ce30_scan_to_frame (scan, frame);
+		int r = fwrite (frame, CE30_WIDTH*CE30_HIEGHT*4, 1, file_out);
+		ASSERT (r == 1);
+		scan.Reset();
 
 
-			if (arg_duration > 0.0)
+
+		struct timespec ts2;
+		clock_gettime (CLOCK_REALTIME, &ts2);
+		d21 = ((ts2.tv_nsec - ts1.tv_nsec) / 1000000000.0) + (ts2.tv_sec  - ts1.tv_sec);
+		ts1 = ts2;
+		d10 = ((ts1.tv_nsec - ts0.tv_nsec) / 1000000000.0) + (ts1.tv_sec  - ts0.tv_sec);
+		counter++;
+
+
+		if (arg_flags & ARG_VERBOSE)
+		{
+			printf ("%10u %10.2lf %10.8lf %10.6lf %10.6lf\n", counter, d10, d21, 1.0 / d21, (double)counter / d10);
+		}
+		if (arg_duration > 0.0 && d10 > arg_duration)
+		{
+			if (arg_flags & ARG_VERBOSE)
 			{
-				clock_gettime (CLOCK_REALTIME, &ts1);
-				double d = ((ts1.tv_nsec - ts0.tv_nsec) / 1000000000.0) + (ts1.tv_sec  - ts0.tv_sec);
-				if (d > arg_duration)
-				{
-					printf ("The recording has ended because it had limited recording duration\n");
-					break;
-				}
+				printf ("%10s %10s %10s %10s %10s\n", "frame", "duration", "spf", "fps", "avg_fps");
 			}
+			printf ("The recording has ended because it had limited recording duration\n");
+			break;
 		}
 	}
 	StopRequestPacket stop_request;
